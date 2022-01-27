cd8fabcbbe3ef65248fb74bfd8564c351d37eece
Bohoaush <sin@boprograms.eu>
2022-01-27 09:35:05 +0100

    segmenting test

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const { exec } = require("child_process");
 
 var delayDivs = 1; //Specified delay in video segments
-var divSize = 60; //Size of video segments in seconds
+var divSize = 10; //Size of video segments in seconds
 
 console.log("Program started fine...");
 
-exec("ffplay /home/bohoaush/Videos/testH265_2M.mp4", (error, stdout, stderr) => {
+//Using test file instead of live input until decklink is available for testing
+exec(("ffmpeg -i /home/bohoaush/Videos/testH265_2M.mp4 -c:v mpeg2video -b:v 50M -flags +ildct+ilme -c:a pcm_s24le -ar 48k -f segment -segment_time " + divSize + " -reset_timestamps 1 segment_%03d.mxf"), (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
