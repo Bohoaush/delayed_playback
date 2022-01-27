51632da409091c1e17bdd932ad899181e85d510c
Bohoaush <sin@boprograms.eu>
2022-01-27 12:01:11 +0100

    CCG connection and playing test

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
+const {CasparCG} = require("casparcg-connection");
 const { exec } = require("child_process");
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename);
 
 var delayDivs = 1; //Specified delay in video segments
 var divSize = 10; //Size of video segments in seconds
 var wait = divSize;
 
+var ccg_ip = "127.0.0.1";
+var ccg_port = 5250;
+
 var playingSegment = 0;
 var segmentPrefix = "segment_";
 
-console.log("Program started fine...");
+console.log("Program started fine in " + appDir + "...");
 
 //Using test file instead of live input until decklink is available for testing
 exec(("ffmpeg -i /home/bohoaush/Videos/testH265_2M.mp4 -c:v mpeg2video -b:v 50M -flags +ildct+ilme -c:a pcm_s24le -ar 48k -f segment -segment_time " + divSize + " -reset_timestamps 1 " + segmentPrefix + "%03d.mxf"), (error, stdout, stderr) => {
@@ -22,7 +28,8 @@ exec(("ffmpeg -i /home/bohoaush/Videos/testH265_2M.mp4 -c:v mpeg2video -b:v 50M 
     console.log(`ffmpeg said: ${stdout}`);
 });
 
-console.log("tst");
+var ccgconn = new CasparCG(ccg_ip, ccg_port);
+setTimeout(function(){ccgconn.play(1, 1, "file://" + appDir + "/segment_000.mxf")}, 20000);
 
 //timer
 setInterval(function(){
